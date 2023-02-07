213821102e926798cc5bace8f8b2e08544c48a67
Sergei <gorodilov.cad@gmail.com>
2023-02-07 17:21:30 +0300

    Added Seminar_4

---
+++ Seminar_4/task_25.py
@@ -0,0 +1,34 @@
+# Задача №25. Решение в группах
+# Напишите программу, которая принимает на вход
+# строку, и отслеживает, сколько раз каждый символ
+# уже встречался. Количество повторов добавляется к
+# символам с помощью постфикса формата _n.
+# Input: a a a b c a a d c d d
+# Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+# Для решения данной задачи используйте функцию
+# .split()
+
+import re
+userInput = "a a a b c a a d c d d"
+userList = re.split("\s", userInput)
+print(userInput)
+
+unique = set()
+for i in userList:
+    unique.add(i)
+print(unique)
+
+dictCounter = {}
+for i in unique:
+    dictCounter[i] = int(0)
+print(dictCounter)
+
+for i in userList:
+    for j in dictCounter:
+        if i == j:
+            if dictCounter[j] > 0:
+                print(f"{j}_{dictCounter[j]}",  end=' ')
+            elif dictCounter[j] == 0:
+                print(j,  end=' ')
+            dictCounter[j] += 1
+
